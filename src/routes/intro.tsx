0f474d818c130e4c2b19edb8b792492604ce2123
niloofar soleimani <soleimaniniloofar95@gmail.com>
2026-09-16 17:46:41 +0330

    edit image intro page

--- src/routes/intro.tsx
+++ src/routes/intro.tsx
@@ -1,7 +1,8 @@
 import { CaretLeft } from "@phosphor-icons/react";
 import { createFileRoute, useNavigate } from "@tanstack/react-router";
 import { Btn } from "../Component/Btn/Btn";
-
+  import goToStar from "../assets/image/go to stars.png"
+   import goal from "../assets/image/Logo.png"
 export const Route = createFileRoute("/intro")({
 	component: RouteComponent,
 });
@@ -13,20 +14,20 @@ function RouteComponent() {
 		navigate({ to: "/home" });
 	};
 	return (
-		<div className=" w-full h-dvh flex flex-col p-4 gap-8 justify-center items-center">
-			<img src="../../src/assets/image/go to stars.png" alt=" موشک فضا" />
-			<div className="flex flex-col text-stone-600">
-				<img src="../../src/assets/image/Logo.png" alt="  لوگو هدف" />
-				<p>وقت رسیدن به اهدافه! 🤩</p>
-			</div>
+    <div className=" w-full h-dvh flex flex-col p-4 gap-8 justify-center items-center">
+      <img src={goToStar} alt=" موشک فضا" />
+      <div className="flex flex-col text-stone-600">
+        <img src={goal} alt="  لوگو هدف" />
+        <p>وقت رسیدن به اهدافه! 🤩</p>
+      </div>
 
-			<Btn
-				IconEnd={CaretLeft}
-				title=" شروع"
-				color="brand"
-				style="filled"
-				onclick={HandleGoToHomePage}
-			/>
-		</div>
-	);
+      <Btn
+        IconEnd={CaretLeft}
+        title=" شروع"
+        color="brand"
+        style="filled"
+        onclick={HandleGoToHomePage}
+      />
+    </div>
+  );
 }
